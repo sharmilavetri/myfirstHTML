4fcc1f6b006e4a7bf8f7a0139170f3a6c1552802
sharmilavetri <sharmilavtrk97@gmail.com>
2024-09-10 18:46:01 +0530

    add my first layout

---
+++ firstlayout.js
@@ -0,0 +1,15 @@
+$(function(){
+
+
+    console.log("DOM ready")
+  
+    $('.slide').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+      });
+    
+
+})
+
+
